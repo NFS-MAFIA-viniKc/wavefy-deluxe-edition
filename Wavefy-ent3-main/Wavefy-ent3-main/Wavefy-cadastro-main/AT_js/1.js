3361729f03581b7e8c85fda08fb5e48423aef993
viniMporto <162714936+NFS-MAFIA-viniKc@users.noreply.github.com>
2024-11-26 08:26:38 -0300

    Add files via upload

---
+++ Wavefy-ent3-main/Wavefy-ent3-main/Wavefy-cadastro-main/AT_js/1.js
@@ -0,0 +1,5 @@
+let estoquep =["camiseta","calça","sapato"];
+estoquep.push("meia");
+estoquep.shift(0);
+estoquep[1]="bermuda";
+console.log(estoquep);
---
+++ Wavefy-ent3-main/Wavefy-ent3-main/Wavefy-cadastro-main/AT_js/2.js
@@ -0,0 +1,7 @@
+let listac=[];
+console.log("Por favor liste 3 itens que você deseja")
+
+if (listac[2]!="leite") {
+    listac.pop("leite")
+    
+}
